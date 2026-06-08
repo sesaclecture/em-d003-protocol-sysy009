12fc41a7308a3856854de5cfeb6d99e0d12d229b
lsy <seongyong0284@gmail.com>
2026-06-08 20:33:37 +0900

    protocol 과제입니다

--- src/main.py
+++ src/main.py
@@ -5,7 +5,11 @@
 # level 값은 0 또는 1입니다.
 # 0이면 "LOW", 1이면 "HIGH"를 반환해야 합니다.
 def gpio_level_to_state(level):
-    raise NotImplementedError
+    if level == 0:
+        return "LOW"
+    else:
+        return "HIGH"
+#    raise NotImplementedError
 
 
 # 문제 2.
@@ -16,7 +20,8 @@ def gpio_level_to_state(level):
 # 송신 데이터는 bytes 타입이어야 합니다.
 # UART 메시지 끝에는 newline 문자가 포함되어야 합니다.
 def make_uart_tx_packet(message):
-    raise NotImplementedError
+    return f"{message}\n".encode('utf-8') 
+    #raise NotImplementedError
 
 
 # 문제 3.
@@ -26,7 +31,8 @@ def make_uart_tx_packet(message):
 # packet은 bytes 타입입니다.
 # 수신 데이터 끝의 newline 문자를 제거하고 문자열로 반환해야 합니다.
 def parse_uart_rx_packet(packet):
-    raise NotImplementedError
+    return packet.decode('utf-8').strip()
+   # raise NotImplementedError
 
 
 # 문제 4.
@@ -36,7 +42,10 @@ def parse_uart_rx_packet(packet):
 # address는 정수입니다.
 # 유효하면 True, 아니면 False를 반환해야 합니다.
 def is_valid_i2c_address(address):
-    raise NotImplementedError
+    if 0<=address<=127:
+        return True
+    else:
+        return False
 
 
 # 문제 5.
@@ -50,4 +59,13 @@ def is_valid_i2c_address(address):
 #
 # [command, payload_length, payload...]
 def make_spi_transfer_frame(command, payload):
-    raise NotImplementedError
+    if 0<=command<=255:
+        pass
+    for int in payload:
+        if 0<=int<=255:
+            pass
+    return [command, len(payload)] + payload
+        
+
+    
+  #  raise NotImplementedError
